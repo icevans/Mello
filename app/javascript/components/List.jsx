0621fc72f457980344fd8c19ba2280d163af7df9
jkulton <jonkulton@gmail.com>
2019-10-30 14:59:32 -0400

    Create List component
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- app/javascript/components/List.jsx
+++ app/javascript/components/List.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+import CardsContainer from './CardsContainer';
+
 const List = (props) => {
   return (
-    <div>{props.list.title}</div>
+    <div className="list-wrapper">
+        <div className="list-background">
+            <div className="list">
+                <a className="more-icon sm-icon" href=""></a>
+                <div>
+                    <p className="list-title">{props.list.title}</p>
+                </div>
+                <div className="add-dropdown add-top">
+                    <div className="card"></div><a className="button">Add</a><i className="x-icon icon"></i>
+                    <div className="add-options"><span>...</span>
+                    </div>
+                </div>
+                <CardsContainer listId={props.list.id} />
+                <div className="add-dropdown add-bottom">
+                    <div className="card"><div className="card-info"></div><textarea name="add-card"></textarea><div className="members"></div></div>
+                    <a className="button">Add</a><i className="x-icon icon"></i>
+                    <div className="add-options"><span>...</span>
+                    </div>
+                </div>
+                <div className="add-card-toggle" data-position="bottom">Add a card...</div>
+            </div>
+        </div>
+    </div>
   );
 };
 
