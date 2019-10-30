f7d1a4a9a18e1017707e563683b73d075c5ad394
jkulton <jonkulton@gmail.com>
2019-10-30 13:57:19 -0400

    Add link to root in main nav
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- app/javascript/components/shared/TopNav.jsx
+++ app/javascript/components/shared/TopNav.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopNav = () => (
   <nav>
       <ul>
         <li className="boards trello-icon icon">
-          <span>Boards</span>
+          <Link to={`/`}>
+            <span>Boards</span>
+          </Link>
         </li>
         <li className="search-container">
           <div className="search search-icon icon"></div>
           <div className="active-search">
             <div>
-              <input type="text" /> 
+              <input type="text" />
             </div>
             <i className="x-icon icon"></i>
             <i className="goto-icon icon"></i>
