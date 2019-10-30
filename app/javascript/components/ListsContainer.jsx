1ff7e9851721be547822a387c4b79df66c19bcba
jkulton <jonkulton@gmail.com>
2019-10-30 14:59:24 -0400

    Create ListsContainer component
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- app/javascript/components/ListsContainer.jsx
+++ app/javascript/components/ListsContainer.jsx
@@ -17,8 +17,16 @@ function mapDispatchToProps(dispatch, ownProps) {
 class ListsContainer extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.lists.map(list => <List key={list.id} list={list} />)}
+      <div id="list-container" className="list-container">
+        <div id="existing-lists" className="existing-lists">
+          {this.props.lists.map(list => <List key={list.id} list={list} />)}
+        </div>
+        <div id="new-list" className="new-list"><span>Add a list...</span>
+          <input type="text" placeholder="Add a list..." />
+          <div>
+            <input type="submit" className="button" value="Save" /><i className="x-icon icon"></i>
+          </div>
+        </div>
       </div>
     );
   }
