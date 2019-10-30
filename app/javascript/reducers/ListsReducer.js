74acf7a64077645bce2517702a8421656198052e
jkulton <jonkulton@gmail.com>
2019-10-30 13:57:06 -0400

    Create lists reducer
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- app/javascript/reducers/ListsReducer.js
+++ app/javascript/reducers/ListsReducer.js
@@ -1,8 +1,9 @@
 export default function listsReducer(state = [], action) {
   if (action.type === 'FETCH_BOARD_SUCCESS') {
+    const excludedLists = state.filter(list => (list.board_id !== action.board.id));
     const { lists, ...newBoardWithoutLists } = action.board;
 
-    return lists;
+    return excludedLists.concat(lists);
   } else {
     return state;
   }
