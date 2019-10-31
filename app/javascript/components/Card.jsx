d48c84dfed03e5079453f80f29bfea8fab06efa7
Bharat Agarwal <bharatagarwal@gmail.com>
2019-10-31 22:30:30 +0530

    Added date display classes for cards Added live icons for card descriptions
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Jon Kulton <jonkulton@gmail.com>

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 
 const Card = (props) => {
+  const dueDateDifference = (due_date) => {
+    const dueDate = moment(due_date);
+    const currentDate = moment(new Date());
+    const difference = dueDate.diff(currentDate, 'days');
+    console.log(difference);
+    return difference;
+  }
+
+  const dueDateClass = () => {
+    const difference = dueDateDifference(props.card.due_date);
+
+    if (difference === 1 || difference === 0) {
+      return 'due-soon';
+    } else if (difference === -1) {
+      return 'overdue-recent';
+    } else if (difference < -1) {
+      return 'overdue';
+    } else {
+      return '';
+    }
+  }  
+
   return (
     <div className="card-background">
       <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
@@ -11,8 +34,15 @@ const Card = (props) => {
           <p>{props.card.title}</p>
         </div>
         <div className="card-icons">
-          <i className="clock-icon sm-icon overdue-recent completed">{props.card.due_date}</i>
-          <i className="description-icon sm-icon"></i>
+          { props.card.due_date ? (
+            <i className={"clock-icon sm-icon " + dueDateClass()}> {moment(props.card.due_date).format('MMM D')}
+          </i>
+          ) : ''}
+    
+          { props.card.description ? (
+            <i className="description-icon sm-icon"></i>
+          ) : ''}
+
           <i className="comment-icon sm-icon"></i>
         </div>
       </div>
@@ -21,3 +51,16 @@ const Card = (props) => {
 }
 
 export default Card;
+
+// render() {
+//   const isLoggedIn = this.state.isLoggedIn;
+//   return (
+//     <div>
+//       {isLoggedIn ? (
+//         <LogoutButton onClick={this.handleLogoutClick} />
+//       ) : (
+//         <LoginButton onClick={this.handleLoginClick} />
+//       )}
+//     </div>
+//   );
+// }
