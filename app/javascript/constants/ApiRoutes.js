9643f324ecf326ab175facb42d3ca58368cc60f1
jkulton <jonkulton@gmail.com>
2019-11-04 15:18:06 -0500

    add api route and action types constants for creating cards
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- app/javascript/constants/ApiRoutes.js
+++ app/javascript/constants/ApiRoutes.js
@@ -3,3 +3,4 @@ export const BOARDS_SHOW_URL = '/api/boards/';
 export const CREATE_BOARD_URL = '/api/boards';
 export const CREATE_LIST_URL = '/api/lists';
 export const UPDATE_LIST_URL = '/api/lists/';
+export const CREATE_CARD_URL = '/api/cards';
